55e89f11e8bc2a303f1efc3505d2ec7b46fba757
maxmkuu-eng <maxmkuu@gmail.com>
2026-09-18 16:45:34 +0300

    Fix Mbozah Firebase Web SDK configuration

--- src/services/firebaseAuth.ts
+++ src/services/firebaseAuth.ts
@@ -9,12 +9,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDW1COIox3js6triFUpVXmYIAuNaUT61bA',
+  apiKey: 'AIzaSyBVYPOIPhEB7xgi7tdGyFFmc4ByTOL8rkE',
   authDomain: 'com-mkuu-ai.firebaseapp.com',
   projectId: 'com-mkuu-ai',
   storageBucket: 'com-mkuu-ai.firebasestorage.app',
   messagingSenderId: '850880444376',
-  appId: '1:850880444376:android:c638e28dc77931b63624ad',
+  appId: '1:850880444376:web:a9db37836f137c873624ad',
+  measurementId: 'G-302DMH1S27',
 };
 
 const app = initializeApp(firebaseConfig);
